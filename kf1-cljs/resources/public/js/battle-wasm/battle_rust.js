df604cce08de9fb399b00c28a2ff28840f8fab14
ArcQ <eddielaw296@gmail.com>
2018-09-29 17:18:49 -0400

    feat: poc add kfcljs to project in migration

---
+++ kf1-cljs/resources/public/js/battle-wasm/battle_rust.js
@@ -0,0 +1,70 @@
+(function() {
+    var wasm;
+    const __exports = {};
+
+
+    let cachedDecoder = new TextDecoder('utf-8');
+
+    let cachegetUint8Memory = null;
+    function getUint8Memory() {
+        if (cachegetUint8Memory === null || cachegetUint8Memory.buffer !== wasm.memory.buffer) {
+            cachegetUint8Memory = new Uint8Array(wasm.memory.buffer);
+        }
+        return cachegetUint8Memory;
+    }
+
+    function getStringFromWasm(ptr, len) {
+        return cachedDecoder.decode(getUint8Memory().subarray(ptr, ptr + len));
+    }
+
+    __exports.__wbg_alert_4afca543c3543396 = function(arg0, arg1) {
+        let varg0 = getStringFromWasm(arg0, arg1);
+        alert(varg0);
+    };
+
+    let cachedEncoder = new TextEncoder('utf-8');
+
+    function passStringToWasm(arg) {
+
+        const buf = cachedEncoder.encode(arg);
+        const ptr = wasm.__wbindgen_malloc(buf.length);
+        getUint8Memory().set(buf, ptr);
+        return [ptr, buf.length];
+    }
+    /**
+    * @param {string} arg0
+    * @returns {void}
+    */
+    __exports.greet = function(arg0) {
+        const [ptr0, len0] = passStringToWasm(arg0);
+        try {
+            return wasm.greet(ptr0, len0);
+
+        } finally {
+            wasm.__wbindgen_free(ptr0, len0 * 1);
+
+        }
+
+    };
+
+    __exports.__wbindgen_throw = function(ptr, len) {
+        throw new Error(getStringFromWasm(ptr, len));
+    };
+
+    function init(wasm_path) {
+        const fetchPromise = fetch(wasm_path);
+        let resultPromise;
+        if (typeof WebAssembly.instantiateStreaming === 'function') {
+            resultPromise = WebAssembly.instantiateStreaming(fetchPromise, { './battle_rust': __exports });
+        } else {
+            resultPromise = fetchPromise
+            .then(response => response.arrayBuffer())
+            .then(buffer => WebAssembly.instantiate(buffer, { './battle_rust': __exports }));
+        }
+        return resultPromise.then(({instance}) => {
+            wasm = init.wasm = instance.exports;
+            return;
+        });
+    };
+    self.wasm_bindgen = Object.assign(init, __exports);
+})();
