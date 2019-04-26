a22a6fbe7b6906cde7f63424aad06a4021ba0bb0
ArcQ <eddielaw296@gmail.com>
2019-04-26 02:29:10 -0400

    feat: interim solution to get obstacles working properly in the correct places

--- js/src/game/scenes/level-one/api.js
+++ js/src/game/scenes/level-one/api.js
@@ -3,13 +3,15 @@ import engine from 'kf-game-engine';
 import { map } from 'rxjs/operators';
 import { actions as levelOneActions } from 'shared/store/level-one/ducks';
 import { TILE_SIZE } from './constants';
+import { getWindow } from 'utils/global';
 
 export function generateGameMap(store) {
+  console.log(engine.web.screen.bounds);
   return () => request(
     '/gamemap/generate',
     {
-      x: parseInt(engine.web.screen.bounds[0] / TILE_SIZE, 10),
-      y: parseInt(engine.web.screen.bounds[1] / TILE_SIZE, 10),
+      x: parseInt(engine.web.screen.bounds[0] / (TILE_SIZE * getWindow().devicePixelRatio), 10),
+      y: parseInt(engine.web.screen.bounds[1] / (TILE_SIZE * getWindow().devicePixelRatio), 10),
     },
   ).pipe(map((res) => {
     store.dispatch(levelOneActions.setGameMap(res));
